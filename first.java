a400fc1ac4a528728239fc158dc2c88914fcf4cc
7Kannayya <yalamanchiliravi69@gmail.com>
2024-02-28 17:04:36 +0530

    This is sum of elements in an array

---
+++ first.java
@@ -0,0 +1,10 @@
+public class Main{
+	public  static void main(String[] args){
+		int sum=0;
+		int a[5]={34,25,12,2,3};
+		for(int i=0;i<a.length;i++){
+			sum=sum+a[i];
+		}
+		System.out.println("Sum="+sum);
+	}
+}
